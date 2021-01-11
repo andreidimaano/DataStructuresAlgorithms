62e2772f8c9c1b06427659ba795d55f07400f10c
Andrei Dimaano <andreiidimaano@gmail.com>
2021-01-11 11:19:07 -0800

    Merge Sorted Array

---
+++ data-structures/arrays/merge/Merge-Sorted-Arrayy/README.md
@@ -0,0 +1,13 @@
+<h1>Merge Sorted Array</h1>
+
+<a href="https://leetcode.com/problems/merge-sorted-array/">Leet Code Link</a>
+
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+
+
+
+
+
+
---
+++ data-structures/arrays/merge/Merge-Sorted-Arrayy/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> nums1_copy = nums1;
+        
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while(j < m && k < n){
+            nums1.at(i++) = (nums1_copy.at(j) < nums2.at(k)) ? nums1_copy.at(j++) : nums2.at(k++);
+        }
+        
+        while(j < m) {
+            nums1.at(i++) = nums1_copy.at(j++);
+        }
+        
+        while(k < n){
+            nums1.at(i++) = nums2.at(k++);
+        }
+    }
+};
