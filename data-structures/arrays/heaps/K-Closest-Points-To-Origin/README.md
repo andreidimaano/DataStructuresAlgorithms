68715d0f10beb1d603cc8b4bd3bf23ee3809c939
Andrei Dimaano <andreiidimaano@gmail.com>
2021-01-12 20:10:32 -0800

    K Closest Points To Origin

---
+++ data-structures/arrays/heaps/K-Closest-Points-To-Origin/README.md
@@ -0,0 +1,26 @@
+<h1>K Closest Points to Origin</h1>
+
+<a href="https://leetcode.com/problems/k-closest-points-to-origin/">Leet Code Link</a>
+
+We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+<h2>Questions</h2>
+
+<ul>
+    <li>What Heap do you need for largest?</li>
+    <li>What is the time complexity for heap solution?</li>
+    <li>What is the time complexity for the sort solution?</li>
+    <li>How do you make a MAX heap in C++</li>
+    <li>How do you make a MIN heap in C++</li>
+</ul>
+
+<h2>Solution</h2>
+
+
+
+
+
---
+++ data-structures/arrays/heaps/K-Closest-Points-To-Origin/solution.cpp
@@ -0,0 +1,32 @@
+struct compare {
+    bool operator()(const pair<int, int> l, const pair<int, int> r) {
+        return l.first < r.first;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, compare> maxHeap;
+        vector<vector<int>> ans;
+        
+        for(unsigned i = 0; i < points.size(); i++) {
+            vector<int> orderedPair = points.at(i);
+            int distance = pow(orderedPair.at(0), 2) + pow(orderedPair.at(1), 2);
+            
+            pair<int, int> currPair(distance, i);
+            
+            maxHeap.push(currPair);
+            if(maxHeap.size() > K) {
+                maxHeap.pop();
+            }
+        }
+        
+        while(K-- > 0) {
+            int index = maxHeap.top().second;
+            maxHeap.pop();
+            ans.push_back(points.at(index));
+        }
+        return ans;
+    }
+};
