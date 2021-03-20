ea0aca09eb6209a1c51e7b4bd4b884070f7eafed
Andrei Dimaano <andreiidimaano@gmail.com>
2021-03-20 13:48:16 -0700

    add linked list struct

--- data-structures/linked-list/src/node.h
+++ data-structures/linked-list/src/node.h
@@ -6,4 +6,13 @@ struct Node {
     Node* next = nullptr;
 };
 
+/* struct Node {
+    int value;
+    Node* next;
+    Node() : value(0), next(nullptr) {} 
+    Node(int x) : value(x), next(nullptr) {} 
+    Node(int x, Node* next) : value(x), next(nullptr) {}
+};
+*/
+
 #endif
