b6521b5f564ccfaadba3d0330c236e8c2c4f9937
Andrei Dimaano <andreiidimaano@gmail.com>
2021-04-06 22:50:40 -0700

    finish week 1

--- data-structures/arrays/c++/wk1.cpp
+++ data-structures/arrays/c++/wk1.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    int* ptr;
-
-    int arr[5] = {0,6,2,3,1};
-
-    ptr = arr;
-
-    for(int i = 0; i < 5; i++) {
-        cout << 12 / *(ptr + (i / 2)) << " ";
+int arrayToInt(const vector<int>& arr) {
+    int ans = 0;
+    int counter = 1;
+    for(int i = arr.size() - 1; i >= 0; i--) {
+        int temp = counter * arr[i];
+        ans += temp;
+        counter *= 10;
     }
+
+    return ans;
 }
+
+int main() {
+    // int* ptr;
+
+    // int arr[5] = {0,6,2,3,1};
+
+    // ptr = arr;
+
+    // for(int i = 0; i < 5; i++) {
+    //     cout << 12 / *(ptr + (i / 2)) << " ";
+    // }
+
+    // vector<int> arr = {1, 2, 3, 5, 6, 1, 2,3}; 
+    // cout << arrayToInt(arr);
+}
+
 
 /*
 ptr = arr; on line 10. This means that ptr is the memory location of arr[0].
